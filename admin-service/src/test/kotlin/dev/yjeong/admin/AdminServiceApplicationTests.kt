b4fac87402082bdf5263c14fc168b77bcdd0fd72
YJeongKim <yjeongisme@gmail.com>
2021-12-15 17:02:13 +0900

    chore: admin-service 프로젝트 생성

---
+++ admin-service/src/test/kotlin/dev/yjeong/admin/AdminServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.yjeong.admin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
