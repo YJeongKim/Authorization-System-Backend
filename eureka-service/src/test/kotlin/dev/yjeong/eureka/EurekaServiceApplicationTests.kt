505168b915ac3b67d19f3d1eafc7ff272e4e0b1c
YJeongKim <yjeongisme@gmail.com>
2021-12-16 14:19:34 +0900

    chore: eureka-service 프로젝트 생성

---
+++ eureka-service/src/test/kotlin/dev/yjeong/eureka/EurekaServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.yjeong.eureka
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
