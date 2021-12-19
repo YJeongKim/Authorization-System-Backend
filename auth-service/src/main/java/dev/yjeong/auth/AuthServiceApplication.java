de27fa9723ba3e74eb4b35e5c777c2cb043667bd
YJeongKim <yjeongisme@gmail.com>
2021-12-19 16:47:12 +0900

    chore: auth-service에 eureka client 활성화

--- auth-service/src/main/java/dev/yjeong/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/dev/yjeong/auth/AuthServiceApplication.java
@@ -2,7 +2,9 @@ package dev.yjeong.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthServiceApplication {
 
---
+++ auth-service/src/main/java/dev/yjeong/auth/controller/AuthController.java
@@ -0,0 +1,7 @@
+package dev.yjeong.auth.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+}
